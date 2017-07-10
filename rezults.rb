3551cbec955c811d9ee059f622c48fe844360f10
sasha <skitale85@gmail.com>
2017-07-10 05:29:35 +0300

    Rewrited if statements in rezult method

--- rezults.rb
+++ rezults.rb
@@ -9,29 +9,31 @@ class Rezults
   end
 
 
-  def get_rezult number
+  def get_rezult(number)
+     return @rezults[0] if number >= 30
+     return @rezults[1] if number.between?(25, 29)
+     return @rezults[2] if number.between?(19, 24)
+     return @rezults[3] if number.between?(14, 18)
+     return @rezults[4] if number.between?(9, 13)
+     return @rezults[5] if number.between?(4, 8)
+     return @rezults[6] if number < 4
+    # when < 30 && number >= 25
+    #   @rezults[1]
+    # elsif number < 25 && number >= 19
+    #   @rezults[2]
 
-    if number >= 30
-      return @rezults[0]
+    # elsif number < 19 && number >= 14
+    #   @rezults[3]
 
-    elsif number < 30 && number >= 25
-      return @rezults[1]
+    # elsif number < 14 && number >= 9
+    #   @rezults[4]
 
-    elsif number < 25 && number >= 19
-      return @rezults[2]
+    # elsif number < 9 && number >= 4
+    #   @rezults[5]
 
-    elsif number < 19 && number >= 14
-      return @rezults[3]
-
-    elsif number < 14 && number >= 9
-      return @rezults[4]
-
-    elsif number < 9 && number >= 4
-      return @rezults[5]
-
-    else
-      return @rezults[6]
-    end
+    # else
+    #   @rezults[6]
+    # end
 
   end
 end
